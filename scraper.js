d3150b7f3b155c51234a301dbebcd8eb05f2acb7
Kevin Pettinger <kevinpettinger@gmail.com>
2021-10-08 21:49:36 -0700

    Split links into separate fields

--- scraper.js
+++ scraper.js
@@ -15,11 +15,11 @@ async function start() {
 
     const mainPageData = await page.$('.col');
 
-    const titleData = await mainPageData.$$('.title');
+    const dateData = await mainPageData.$$('.title');
     const nftData = await mainPageData.$$('.table-responsive');
-    const titles = await Promise.map(titleData, async (t) => {
-      const title = await t.evaluate((el) => el.textContent);
-      return title.trim();
+    const dates = await Promise.map(dateData, async (t) => {
+      const date = await t.evaluate((el) => el.textContent);
+      return date.trim();
     });
     const headerDataRow = await nftData[0].$('thead tr');
     const headerDataRowTitles = await headerDataRow.$$('th');
@@ -27,16 +27,21 @@ async function start() {
       async (t) => t.evaluate((el) => el.textContent.trim()));
     const nfts = [];
     await Promise.map(nftData, async (day, idx) => {
-      const nftDate = titles[idx];
+      const nftDate = dates[idx];
       const nftsByDayRows = await day.$$('tbody tr');
       const nftByDay = await Promise.map(nftsByDayRows, async (row) => {
-        const currentNft = {};
-        currentNft.Date = nftDate;
+        const currentNft =
+          { Home: '', Twitter: '', Discord: '', Date: nftDate };
+
         const rowData = await row.$$('td');
         currentNft[headers[0]] = await rowData[0].evaluate((el) => el.textContent.trim());
         const linkData = await rowData[1].$$('a');
         const links = await Promise.map(linkData, async (l) => l.evaluate((el) => el.href));
-        currentNft[headers[1]] = links.join(' ');
+        links.forEach((link) => {
+          if (link.toLowerCase().includes('twitter')) currentNft.Twitter = link;
+          else if (link.toLowerCase().includes('discord')) currentNft.Discord = link;
+          else currentNft.Home = link;
+        });
         Promise.each(rowData.slice(2), async (data, i) => {
           currentNft[headers[i + 2]] = await data.evaluate((el) => el.textContent.trim());
         });
